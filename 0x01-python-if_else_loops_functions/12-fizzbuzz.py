f1e89fe4f08474c1a3f2a6d1816f710f54abeea6
dimka90 <dimkayilrit@gmail.com>
2023-08-08 19:41:12 +0000

    added the zero to the equals sign on three

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for counter in range(1, 101):
-        if counter % 3 and counter % 5 == 0:
+        if counter % 3 == 0 and counter % 5 == 0:
             print("FizzBuzz ", end="")
         elif counter % 3 == 0:
             print("Fizz ", end="")
@@ -9,3 +9,4 @@ def fizzbuzz():
             print("Buzz ", end="")
         else:
             print("{} ".format(counter), end="")
+fizzbuzz()
