32a783873d2f0118a89f03603918605b7c12ef7a
dimka90 <dimkayilrit@gmail.com>
2023-08-08 19:38:23 +0000

    fix the Fizzbuzz error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for counter in range(1, 101):
-        if counter % 3 == 0:
+        if counter % 3 and counter % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif counter % 3 == 0:
             print("Fizz ", end="")
         elif counter % 5 == 0:
             print("Buzz ", end="")
-        elif counter % 5 == 0 and counter % 3 == 0:
-            print("FizzBuzz ", end="")
         else:
             print("{} ".format(counter), end="")
