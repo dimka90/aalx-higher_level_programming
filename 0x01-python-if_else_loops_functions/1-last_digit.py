6aa4f3ae81cb3a2a0d1ee71dee283f8ba5638bd0
dimka90 <dimkayilrit@gmail.com>
2023-08-08 13:09:15 +0000

    fix the bug by changing the lowercase letter l to capital L

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ remainder = abs(number) % 10
 if remainder > 5:
     print(f"Last digit of {number:d} is {remainder:d} and is greater than 5")
 elif remainder == 0:
-    print(f"last digit of {number:d} is {remainder:d} and is 0")
+    print(f"Last digit of {number:d} is {remainder:d} and is 0")
 elif remainder < 6 and remainder != 0:
     print(f"Last digit of {number:d} is {remainder:d}\
     and is less than 6 and not 0")
