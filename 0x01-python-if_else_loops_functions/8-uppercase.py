3638350f58f284977fcfdb20615f2b27965aebac
dimka90 <dimkayilrit@gmail.com>
2023-08-08 14:54:02 +0000

    remove the third print function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    str_len = len(str)
     for counter in str:
         if (ord(counter) >= 97 and ord(counter) <= 122):
             counter = ord(counter) - 32
             print("{:c}".format(counter), end="")
         else:
             print("{}".format(counter), end="")
-    print("")
