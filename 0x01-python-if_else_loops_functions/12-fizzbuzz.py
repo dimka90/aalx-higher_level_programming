784f6243799aea5cd4467b8550ef9dae6fc4c843
dimka90 <dimkayilrit@gmail.com>
2023-08-08 19:46:39 +0000

    Remove the function call

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,10 @@
 def fizzbuzz():
     for counter in range(1, 101):
         if counter % 3 == 0 and counter % 5 == 0:
-            print("FizzBuzz ", end="")
+            print("FizzBuzz", end=" ")
         elif counter % 3 == 0:
-            print("Fizz ", end="")
+            print("Fizz", end=" ")
         elif counter % 5 == 0:
-            print("Buzz ", end="")
+            print("Buzz", end=" ")
         else:
-            print("{} ".format(counter), end="")
-fizzbuzz()
+            print("{}".format(counter), end=" ")
