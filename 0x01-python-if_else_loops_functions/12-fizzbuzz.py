558c57b694bc49fa43c4a709c1b2afcec8e77b85
dimka90 <dimkayilrit@gmail.com>
2023-08-08 19:24:09 +0000

    Added a function that print fizz, buzz or fizzbuzz for 3, 5 or both

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for counter in range(1, 101):
         if counter % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz ", end="")
         elif counter % 5 == 0:
-            print("Buzz ", end=" ")
+            print("Buzz ", end="")
         elif counter % 5 == 0 and counter % 3 == 0:
-            print("FizzBuzz ", end=" ")
+            print("FizzBuzz ", end="")
         else:
-            print("{} ".format(counter), end=" ")
+            print("{} ".format(counter), end="")
