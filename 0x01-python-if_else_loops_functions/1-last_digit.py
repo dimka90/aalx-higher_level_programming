c7d81a8b34843555cae726df78f7c1ff30b11439
dimka90 <dimkayilrit@gmail.com>
2023-08-08 16:09:02 +0000

    Fix pycodestyle error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 remainder = abs(number) % 10
 if number < 0:
- remainder = - remainder
+    remainder = - remainder
 if remainder > 5:
     print(f"Last digit of {number:d} is {remainder:d} and is greater than 5")
 elif remainder == 0:
