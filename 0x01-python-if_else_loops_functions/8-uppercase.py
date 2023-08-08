be324e9e1a94198fb95214f8d38c9c5afedadf0c
dimka90 <dimkayilrit@gmail.com>
2023-08-08 15:17:20 +0000

    Added a newline character at the end of the string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
     str_len = len(str)
+    upper = ""
     for counter in str:
         if (ord(counter) >= 97 and ord(counter) <= 122):
             counter = ord(counter) - 32
-            print("{:c}".format(counter), end="")
+            upper = upper + "{:c}".format(counter)
         else:
-            print("{}".format(counter), end="")
+            upper = upper + "{}".format(counter)
+    print(upper)
