f1a3d42c637fd2f54e931cf3d8ba1a4091596036
dimka90 <dimkayilrit@gmail.com>
2023-08-08 16:07:58 +0000

    Added the negative sign on the in task one

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 remainder = abs(number) % 10
+if number < 0:
+ remainder = - remainder
 if remainder > 5:
     print(f"Last digit of {number:d} is {remainder:d} and is greater than 5")
 elif remainder == 0:
