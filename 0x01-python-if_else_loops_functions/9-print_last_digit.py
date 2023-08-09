a1188d0a50d8743fa27c1015beb0246c5c2733a1
dimka90 <dimkayilrit@gmail.com>
2023-08-09 03:43:12 +0000

    Fix task 9 bug

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     abs_value = abs(number) % 10
-    if number < 0:
-        print("{0:d}".format(- abs_value))
-        return - abs_value
-    else:
-        print("{0:d}".format(abs_value))
-        return abs_value
+    print("{}".format(abs_value), end="")
+    return abs_value
