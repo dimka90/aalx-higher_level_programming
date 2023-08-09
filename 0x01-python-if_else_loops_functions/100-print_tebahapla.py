5e828106babcb2433d597319d75dacef4d22ffa7
dimka90 <dimkayilrit@gmail.com>
2023-08-09 03:25:29 +0000

    Added a function that prints strings in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahapla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahapla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-diff = 0
+i = 0
 for counter in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(counter - i)), end="")
-    diff = 32 if count == 0 else 0
+    i = 32 if i == 0 else 0
