0627835826bde78f700e2af611b5860a4ec6458d
dimka90 <dimkayilrit@gmail.com>
2023-08-10 14:39:32 +0000

    Added a function that prints the number of command line element entered

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    """ Command line function
+    Args: none
+    Return: none
+    """
+    count = 1
+    argv_len = len(sys.argv) - 1
+    if argv_len == 0:
+        print("{} arguments.".format(argv_len))
+    elif argv_len == 1:
+        print("{} argument:".format(argv_len))
+    else:
+        print("{} arguments:".format(argv_len))
+    for counter in sys.argv[1:]:
+        print("{}: {}".format(count, counter))
+        count = count + 1
+
+
+if __name__ == "__main__":
+    main()
