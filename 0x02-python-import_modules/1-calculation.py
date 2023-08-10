30259f368e0627e532109cf2fae0a0f8c53743f7
dimka90 <dimkayilrit@gmail.com>
2023-08-10 15:05:45 +0000

    Fix task one bug by putting the whole code in a function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import calculator_1
-a = 10
-b = 5
 
-if __name__ == "__main__":
+
+def main():
+    a = 10
+    b = 5
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
     print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
-    
+
+
+if __name__ == '__main__':
+    main()
