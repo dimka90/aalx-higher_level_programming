f1a88aea2ac19bc34313da1905653d488a16d3f5
dimka90 <dimkayilrit@gmail.com>
2023-08-10 12:58:10 +0000

    Added a function that prints the sum of tw numbers

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import add_0
+# this program print the sum of two numbers
+a = 1
+b = 2
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,2 @@
+ Python - import & modules
+import is a way of putting another code in the code you want to use  
