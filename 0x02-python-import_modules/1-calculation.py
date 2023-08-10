de72b744242f78dd117373adfcd56ee9cd2ba5bd
dimka90 <dimkayilrit@gmail.com>
2023-08-10 15:05:45 +0000

    convert each string to int

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ import calculator_1 as cal
 def main():
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, cal.add(a, b)))
-    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
-    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
-    print("{} / {} = {}".format(a, b, cal.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
 
 
 if __name__ == '__main__':
