335847f9d58e74715093b53e9eb666adc993c6ce
dimka90 <dimkayilrit@gmail.com>
2023-08-11 15:51:05 +0000

    Added a function that create and modify a new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list: list, idx: int, element: int) -> list:
+    """create a new list and modify it the new list"""
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    # slice[:] create a new copy of the list in memory
+    # the contain of the new list is copied to the new list
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
