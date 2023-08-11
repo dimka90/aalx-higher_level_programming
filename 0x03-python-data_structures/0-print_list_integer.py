346f23d4ca878a9f1df7f608755601944ace49a7
dimka90 <dimkayilrit@gmail.com>
2023-08-11 14:43:42 +0000

    Fix indentation error and converted the str format to display interger

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-   for counter in my_list:
-        print("{}".format(counter))
-
-
+    """
+    This function print the element found in  list
+    """
+    for counter in my_list:
+        print("{:d}".format(counter))
