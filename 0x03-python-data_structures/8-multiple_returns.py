7b7625ef6c1faa32c85595435ce7043d16997bde
dimka90 <dimkayilrit@gmail.com>
2023-08-12 13:35:12 +0000

    fix some bug

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence: str) -> tuple:
     char = sentence[0]
     """This program returns the length of string and the first letter"""
-    if len(sentence) == 0:
+    if sentence == "":
         return (0, None)
     return (len(sentence), char)
