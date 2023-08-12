bc0ffd84ab84da3922e7807bdf97909f85148385
dimka90 <dimkayilrit@gmail.com>
2023-08-12 13:29:18 +0000

    fix PEP errror

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence: str) -> tuple:
     """This program returns the length of string and the first letter"""
     char = sentence[0]
     if len(sentence) == "":
-        return (0,None)
+        return (0, None)
     return (len(sentence), char)
