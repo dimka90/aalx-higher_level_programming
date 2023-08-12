b88db3a5bef66dc796d901721ee8a737b5d92ad3
dimka90 <dimkayilrit@gmail.com>
2023-08-12 11:59:21 +0000

    remove the int format in task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]) -> None:
     """This function print a list in reverse order"""
     list_len = len(my_list) - 1
     for counter in range(list_len, -1, -1):
-        print("{:d}".format(my_list[counter]))
+        print("{}".format(my_list[counter]))
