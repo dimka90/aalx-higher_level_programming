6ef3cb85fe7cd868c42b4cf548595f397eab78a2
dimka90 <dimkayilrit@gmail.com>
2023-08-12 12:20:12 +0000

    fix task 6 bug

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]) -> None:
     """This program print the a 2 X 2 matrix """
-    for counter in matrix:
-        for i in counter:
-            print(" {:d}".format(i), end="")
-        print("")
+    for row in matrix:
+        for column in row:
+            print("{:d}".format(column), end=" " if column != row[-1] else "")
+        print()
