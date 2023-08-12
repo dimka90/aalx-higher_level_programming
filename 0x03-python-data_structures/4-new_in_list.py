cd4e5aa1e1ed15803062d0ececf8d5ba99dbd1f0
dimka90 <dimkayilrit@gmail.com>
2023-08-12 12:00:30 +0000

    fix a bug in task 4 by using a >= condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list: list, idx: int, element: int) -> list:
     """create a new list and modify it the new list"""
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     # slice[:] create a new copy of the list in memory
     # the contain of the new list is copied to the new list
