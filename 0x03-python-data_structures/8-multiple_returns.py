810d9b5111b11ad060095d670f51c6b0a713eb72
dimka90 <dimkayilrit@gmail.com>
2023-08-14 14:51:43 +0000

    fix task 8 error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence: str) -> tuple:
-    char = sentence[0]
     """This program returns the length of string and the first letter"""
+    char = sentence[0]
     if sentence == "":
         return (0, None)
     return (len(sentence), char)
