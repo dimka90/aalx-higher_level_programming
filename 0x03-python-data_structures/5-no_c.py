daa22c4feb2c532e9e63f66b38a4a19fd1ff817c
dimka90 <dimkayilrit@gmail.com>
2023-08-14 14:44:36 +0000

    added a fuction that remove c and C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """Removes all occurrences of characters 'c' and 'C' from the string."""
+    result = []
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result.append(char)
+    return ''.join(result))
