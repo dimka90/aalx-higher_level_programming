235afa36d790ec8d1c8c9c5c41d7f5f17f0be499
dimka90 <dimkayilrit@gmail.com>
2023-08-14 14:41:46 +0000

    fix the bug

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,3 @@ def print_reversed_list_integer(my_list=[]):
         list_len = len(my_list)
         for i in range(list_len - 1, -1, -1):
             print("{:d}".format(my_list[i]))
-    else:
-        print("Input is not a list.")
