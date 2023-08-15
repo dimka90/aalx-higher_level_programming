a2318fe7d961afcadc8bb2bd1a54eeaa0d4c3d8a
dimka90 <dimkayilrit@gmail.com>
2023-08-15 02:31:18 +0000

    fix task eight bug

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,7 @@
+#include <python3.8/Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+
+
+}
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence: str) -> tuple:
     """This program returns the length of string and the first letter"""
-    char = sentence[0]
-    if sentence == "":
+    if len(sentence) == 0:
         return (0, None)
-    return (len(sentence), char)
+    return (len(sentence), sentence[0])
