b5281be52e4146e4ac04d3c5cbe034de6557bd8c
dimka90 <dimkayilrit@gmail.com>
2023-08-15 02:47:36 +0000

    fix task 7 bug

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()) -> tuple:
     """This program takes in two tuples and add them together"""
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = tuple_a + (0, 0)
-        elif len(tuple_b) == 0:
-            tuple_b = tuple_b + (0, 0)
-        elif len(tuple_a) == 1:
-            tuple_a = tuple_a + (0,)
-        elif len(tuple_b) == 1:
-            tuple_b = tuple_b + (0,)
-        else:
-            pass
-    if len(tuple_a) > 2:
-        tuple_a = tuple_a[:2]
-    if len(tuple_b) > 2:
-        tuple_b = tuple_b[:2]
-    result = ()
-    for counter in range(len(tuple_a)):
-        element1 = tuple_a[counter]
-        element2 = tuple_b[counter]
-        add = element1 + element2
-        result = result + (add,)
+    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+    # Take only the first two elements of each tuple and add them
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result
