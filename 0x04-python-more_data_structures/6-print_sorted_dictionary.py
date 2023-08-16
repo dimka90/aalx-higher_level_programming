90239e98f046d16072cccf83c2fdc4d2c853620c
dimka90 <dimkayilrit@gmail.com>
2023-08-16 14:41:44 +0000

    Added annotation for task 6 and 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
+def print_sorted_dictionary(a_dictionary: dict) -> None:
+    """Added a fuction that print a dictionary in sorted form"""
     if isinstance(a_dictionary, dict):
         sorted_keys = sorted(a_dictionary.keys())
         for key in sorted_keys:
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
+def update_dictionary(a_dictionary: dict, key: str, value) -> dict:
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
