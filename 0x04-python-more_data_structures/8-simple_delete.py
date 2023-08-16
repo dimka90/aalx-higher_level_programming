0976916a4f6d8f437e907659d84c92821b57bf68
dimka90 <dimkayilrit@gmail.com>
2023-08-16 14:48:43 +0000

    added a return value for task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,4 +2,5 @@
 def simple_delete(a_dictionary: dict, key="") -> None:
     """ Deleted an element from a dictionary"""
     if key in a_dictionary:
-        del dictionary[key]
+        del a_dictionary[key]
+    return a_dictionary
