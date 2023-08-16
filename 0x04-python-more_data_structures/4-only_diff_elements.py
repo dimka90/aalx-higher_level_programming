e2f13b2abc090313be6fc10ed3b1a230ade55f5d
dimka90 <dimkayilrit@gmail.com>
2023-08-16 13:39:01 +0000

    Added a function that returns a set of all elements present in only one set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1: set, set_2: set) -> set:
-    """ Returns the union of sets"""
-    return (set_1 | set_2)
+    """A function that returns a set of all elements present in only one set"""
+    new_list = set_1.symmetric_difference(set_2)
+    return new_list
