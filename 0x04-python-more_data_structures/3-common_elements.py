3159498efbba9b38c437ba18da1fe911078ebf5f
dimka90 <dimkayilrit@gmail.com>
2023-08-16 12:38:03 +0000

    added a fuction that returns intersection of set

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def common_elements(set_1: set, set_2: set) -> set:
+    """Return a new  set using intersection of set"""
+    return (set_1 & set_2)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,4 @@
+0x04. Python - More Data Structures: Set, Dictionary
+continuation of python data structures 
+set: a data structure that contained unorder data
+
