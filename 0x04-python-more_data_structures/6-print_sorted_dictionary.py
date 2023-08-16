61c79174826c6bd784177b2a74c0a705340debf1
dimka90 <dimkayilrit@gmail.com>
2023-08-16 14:37:13 +0000

    fix task 6 and 7 error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(dictionary) -> None:
-    def custom_sort_key(item):
-        value = item[1]
-        return (type(value).__name__, value)
-    sorted_items = sorted(dictionary.items(), key=custom_sort_key)
-
-    for key, value in sorted_items:
-        print("{}: {}".format(key, value))
+def print_sorted_dictionary(a_dictionary):
+    if isinstance(a_dictionary, dict):
+        sorted_keys = sorted(a_dictionary.keys())
+        for key in sorted_keys:
+            value = a_dictionary[key]
+            print(f"{key}: {value}")
