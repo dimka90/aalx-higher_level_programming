7d3fa0d197270f615dafa052b3e711677175ffdd
dimka90 <dimkayilrit@gmail.com>
2023-08-16 16:09:32 +0000

    fix some bug on task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0) -> set:
-    """Returns the multipy a new list by multiple of 2"""
     return (list(map(lambda n: n * number, my_list)))
