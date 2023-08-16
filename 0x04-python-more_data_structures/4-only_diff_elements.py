ff32b8fda1e51357dc271e5fd16e0db7347e5c91
dimka90 <dimkayilrit@gmail.com>
2023-08-16 13:25:45 +0000

    fix the pydoc stytle for task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1: set, set_2: set):
-    """ Returns the difference between two sets"""
+def only_diff_elements(set_1: set, set_2: set) -> set:
+    """ Returns the union of sets"""
     return (set_1 | set_2)
