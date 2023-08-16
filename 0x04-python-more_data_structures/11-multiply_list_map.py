4ab736e90ea78647abbbaa538016eedb8349c2dc
dimka90 <dimkayilrit@gmail.com>
2023-08-16 16:08:26 +0000

    fix task 11 error

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0) -> set:
     """Returns the multipy a new list by multiple of 2"""
-    mul_2 = list(map(lambda n: n * number, my_list))
-    return mul_2
+    return (list(map(lambda n: n * number, my_list)))
