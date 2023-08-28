4e899dd0974ec609c110da865dc663016946b9b2
dimka90 <dimkayilrit@gmail.com>
2023-08-28 17:23:04 +0000

    added a function that takes the address of another function and execute it

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args) -> float, int or None:
+    """
+    a function that executes a function safely.
+    This program takes in a function and try to
+    execute the function
+    """
+    try:
+        result = fct(*args)
+        return result
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        result = None
+        return result
+    except ZeroDivisionError as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        result = None
+        return result
+    except IndexError as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        result = None
+        return result
