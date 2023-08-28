ce185cbd06bf97a88b89145907567bef12561d55
dimka90 <dimkayilrit@gmail.com>
2023-08-28 16:24:51 +0000

    fix a bug in task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,8 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(num), end="")
                 count = count + 1
         print()
-    except IndexError:
-        print("index error")
     except ValueError:
-        print("Not an int")
+        pass
     return count
