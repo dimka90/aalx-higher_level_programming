90c1c64cbe28b08e124e4981375b0bb6857b0e6c
dimka90 <dimkayilrit@gmail.com>
2023-08-28 16:56:41 +0000

    Fix task one error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,5 +6,5 @@ def safe_print_integer(value) -> bool:
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
