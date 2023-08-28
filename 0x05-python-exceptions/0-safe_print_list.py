089c7dee3beb25404838db319afb417a92bfad9b
dimka90 <dimkayilrit@gmail.com>
2023-08-28 13:24:14 +0000

    Added a function that print the element of alist from a specific point

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """a function that prints a specify  elements of a list"""
+    try:
+        count = 0
+        new_list = my_list[:x]
+        [print(num, end="") for num in new_list]
+        print()
+        for count in new_list:
+            count = count + 1
+    except TypeError:
+        print("invalid type")
+    return count
