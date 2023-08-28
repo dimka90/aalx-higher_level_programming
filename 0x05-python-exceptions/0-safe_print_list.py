2de2edbd5273638941081a57051d00f7d582362c
dimka90 <dimkayilrit@gmail.com>
2023-08-28 13:43:41 +0000

    fix a bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0) -> int:
     """a function that prints a specify  elements of a list"""
     try:
         count = 0
         new_list = my_list[:x]
-        [print(num, end="") for num in new_list]
-        print()
-        for count in new_list:
+        [print("{0:d}".format(num), end="") for num in new_list]
+        for num in new_list:
             count = count + 1
+        print()
     except (TypeError, ValuError, keyboardInterrupt):
         print("do not interupt")
-    return count - 1
+    return count
