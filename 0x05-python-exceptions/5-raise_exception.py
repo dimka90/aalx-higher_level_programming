36c3a76476d3c992ca42f34a1c9cb6eeea230b99
dimka90 <dimkayilrit@gmail.com>
2023-08-28 15:55:06 +0000

    fix some declaration in the function

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -4,8 +4,4 @@ def raise_exception() -> None:
      a function that raises a type exception
      Return: None
     """
-    try:
-        result = 10 / 0
-        raise ZeroDivisionError("Dvision by zero")
-    except ZeroDivisionError as diverror:
-        print("Divison:", diverror)
+    raise TypeError("Exception occured")
