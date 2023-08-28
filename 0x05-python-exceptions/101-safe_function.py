6b5ab531e32d5eaf01811c381c88b35969154b62
dimka90 <dimkayilrit@gmail.com>
2023-08-28 17:26:58 +0000

    fix some bug

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -13,13 +13,10 @@ def safe_function(fct, *args) -> float, int or None:
         return result
     except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        result = None
-        return result
+        return None
     except ZeroDivisionError as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        result = None
-        return result
+        return None
     except IndexError as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        result = None
-        return result
+        return None
