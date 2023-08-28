c047ee47d42d1ea593015b843d25ec1122aa87e3
dimka90 <dimkayilrit@gmail.com>
2023-08-28 13:34:21 +0000

    Added some exception clause

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
         print()
         for count in new_list:
             count = count + 1
-    except:
-        pass
+    except (TypeError, ValuError, keyboardInterrupt):
+        print("do not interupt")
     return count - 1
