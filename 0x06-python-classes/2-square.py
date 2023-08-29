f059aaf47cd5f722cf69da17a1e396ebba6ab12b
dimka90 <dimkayilrit@gmail.com>
2023-08-29 14:45:14 +0000

    fix all strings error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,7 +16,7 @@ class Square:
              None
          """
         if not isinstance(size, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
