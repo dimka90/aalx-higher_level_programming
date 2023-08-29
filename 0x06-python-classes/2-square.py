45e9844ba27c037f4426d1a1ce03dcbce33de591
dimka90 <dimkayilrit@gmail.com>
2023-08-29 14:30:44 +0000

    Added a function validate the size of a Square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+"""
+Template of  square class
+"""
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size=0) -> None:
+        """
+         Args:
+             size (int): a private attribute that keeps the size of the square
+         Returns:
+             None
+         """
+        self.__size = size
+
+    @size.setter
+    def size(self, size) -> None:
+        """
+        Args:
+             size (int): a private attribute that
+        Returns:
+                None
+        """
+        if is not instance(size, int):
+            raise TypeError("size must ne an interger")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
