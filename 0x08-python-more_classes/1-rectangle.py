8224e455df50ffe4fa35887f31bd29a67668ae39
dimka90 <dimkayilrit@gmail.com>
2023-09-04 14:21:50 +0000

    Fix a bug

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
         Returns:
               None
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -34,12 +34,10 @@ class Rectangle:
                    self(obeject): instance of the new class
                    value(int) : width of the rectangle
         """
-
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be >= 0")
-
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -59,10 +57,8 @@ class Rectangle:
                    self(obeject): instance of the new class
                    value(int) : height of the rectangle
         """
-
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
