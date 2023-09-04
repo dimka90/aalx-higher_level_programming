eda51c28cf94f95b3bbabcc1f07d82f27aeddd7b
dimka90 <dimkayilrit@gmail.com>
2023-09-04 14:18:01 +0000

    fix a bug

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,52 +17,52 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """
+    @property
+    def width(self):
+        """
             This function returns the width of the rectangle
             Args:
                  self(obeject): instance of the new class
-            """
-            return self.__width
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """
+    @width.setter
+    def width(self, value):
+        """
               This function returns the width of the rectangle
               Args:
                    self(obeject): instance of the new class
                    value(int) : width of the rectangle
-             """
+        """
 
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise TypeError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
 
-            self.__width = value
+        self.__width = value
 
-        @property
-        def height(self):
-            """
+    @property
+    def height(self):
+        """
             This function returns the width of the rectangle
             Args:
                  self(obeject): instance of the new class
-            """
-            return self.__height
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
+    @height.setter
+    def height(self, value):
+        """
               This function returns the width of the rectangle
               Args:
                    self(obeject): instance of the new class
                    value(int) : height of the rectangle
-             """
+        """
 
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
 
-            self.__height = value
+        self.__height = value
