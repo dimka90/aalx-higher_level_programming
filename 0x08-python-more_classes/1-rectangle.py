a736d9bba487124f3c369b3a366716ff99e249be
dimka90 <dimkayilrit@gmail.com>
2023-09-04 14:03:56 +0000

    fix a bug in task one

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
         Returns:
               None
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
         @property
         def width(self):
@@ -63,6 +63,6 @@ class Rectangle:
             if not isinstance(value, int):
                 raise TypeError("height must be an integer")
             if value < 0:
-                raise TypeError("height must be >= 0")
+                raise ValueError("height must be >= 0")
 
             self.__height = value
