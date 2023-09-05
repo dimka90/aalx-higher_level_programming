63208492b7c1c7c6e4e4244de4b39df2aa69a175
dimka90 <dimkayilrit@gmail.com>
2023-09-05 13:39:33 +0000

    fix some major bug in the task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,3 +7,11 @@ class LockedClass:
     Only the creation of obejct only if the attribute is firstname
     """
     __slots__ = ('firstname',)
+
+    def __init__(self):
+        """
+           Create an instance of the Lockedclass
+           Args:
+              self(object) : instance of the newly created object
+        """
+        self.firstname = 'firstname'
