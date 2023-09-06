ba2feee49ddc7dd4a3c642bab274239bc9ac0a63
dimka90 <dimkayilrit@gmail.com>
2023-09-06 13:45:46 +0000

    fix a bug in task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,8 +14,8 @@ def print_square(size):
 
     if (size < 0) and isinstance(size, float) or size < 0:
         raise TypeError("size must be >= 0")
-    if size == 0:
-        print()
+    #if size == 0:
+    #   print()
     # printing each element in 2d
     for i in range(0, size):
         for x in range(0, size):
