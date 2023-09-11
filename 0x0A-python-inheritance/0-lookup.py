23da913ab5efb3f0d82f8bfdd83aa04a6ab0201b
dimka90 <dimkayilrit@gmail.com>
2023-09-11 12:45:55 +0000

    fix a bug

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,11 +3,17 @@
 Returns the object of a list
 """
 
+
 def lookup(obj):
     """
      A function that returns the list
       and method of an object
     Args:
-        obj(object): object variable 
+        obj(object): object variable
+
+    """
+    """
+    attributes_and_methods = dir(obj)
+    return filtered_attributes_and_methods
     """
     return dir(obj)
