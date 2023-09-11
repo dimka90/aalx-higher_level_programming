8577c2c92777447250abd4eda0feb5260ea3136a
dimka90 <dimkayilrit@gmail.com>
2023-09-11 18:28:20 +0000

    Added an class function that is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Empty Class of Shapes"""
+
+
+class BaseGeometry:
+    """ A class for all shape"""
+    pass
+
+    def area(self):
+        """
+        Area is not yet implemented
+        """
+        raise Exception("200~area() is not implemented")
