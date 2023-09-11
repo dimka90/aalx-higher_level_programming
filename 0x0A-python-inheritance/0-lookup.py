5f488d4949d4999c7e397a8f3230afef604e1781
dimka90 <dimkayilrit@gmail.com>
2023-09-11 13:35:30 +0100

    added readme file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Returns the object of a list
+"""
+
+def lookup(obj):
+    """
+     A function that returns the list
+      and method of an object
+    Args:
+        obj(object): object variable 
+    """
+    return dir(obj)
