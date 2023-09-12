2f9f5b5a525d161c10c8dc3b5be22bdaf4a4aa67
dimka90 <dimkayilrit@gmail.com>
2023-09-12 02:16:38 +0000

    Added A rectangular class, derived from the Basedgeometry class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module that for Rectangular shape."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A Rectangle class"""
+
+    def __init__(self, width, height):
+        """
+           A function that performs a check before initialisation
+           Args:
+               width(int): width of the rectangle
+               height(int): height of the rectangle
+           Returns:
+                  None
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
