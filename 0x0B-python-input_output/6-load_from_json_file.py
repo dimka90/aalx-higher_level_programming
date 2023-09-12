32f515a1bedec321c5af4cf92d62d25a25a8c26f
dimka90 <dimkayilrit@gmail.com>
2023-09-12 13:33:50 +0000

    Added a command to task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,6 +10,7 @@ def load_from_json_file(filename):
     A function that saves a python object to a file
     Args:
         filename(str): file name
+        Returns: The json object represtation of json file
     """
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
