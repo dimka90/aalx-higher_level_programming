4f0602e713a10a873601c06f0b62070e5fbab3aa
dimka90 <dimkayilrit@gmail.com>
2023-09-15 14:12:36 +0000

    Adde a function A Rectangle child which is a subclass for the Base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,80 @@ from models.base import Base
 class Rectangle(Base):
     """
     A Rectangle classs
-    Arg: 
+    Arg:
        Base(Class): Parent class of rectangle
     """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        instantiation of vairable
+        args:
+            self(object): object of the Class Rectangle
+            width:(int): width of the rectangle
+            height(int): height of the rectangle
+            x(int)
+            y(int):
+            id(int) or None for Default
+        Return:
+               None
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """
+        returns the width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """
+        set the width property of the rectangle
+        """
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+        returns the height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """
+         set the height property of the rectangle
+        """
+        self.__height = height
+
+    @property
+    def x(self):
+        """
+        returns the width of the rectangle
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """
+         set the x property of the rectangle
+        """
+        self.__x = x
+
+    @property
+    def y(self):
+        """
+        returns the y of the rectangle
+        """
+        return self.__y
+
+    @y.setter
+    def height(self, y):
+        """
+         set the y property of the rectangle
+        """
+        self.__y = y
