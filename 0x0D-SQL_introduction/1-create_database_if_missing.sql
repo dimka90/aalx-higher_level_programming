568995592a982f04390564a81dfd3c3e3305c96a
dimka90 <dimkayilrit@gmail.com>
2023-10-17 13:43:10 +0100

    fix syntax error on sql satement,change EXIT TO EXISTS

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- a script that creata a database,if it did not exist
 -- Using the if conditional to check for the existence of the database
-CREATE DATABASE IF NOT EXIT hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
