5d373db74c8ff3954e64524c5bb0508ee073dd2c
dimka90 <dimkayilrit@gmail.com>
2023-10-18 14:12:07 +0100

    a script that creates the database hbtn_0d_2 and the user user_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2
+-- Creating a new database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Creating a new user if the user  does not exit with  password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Granting privillages the select privillages to the newly created user
+GRANT SELECT  ON htb_0d_2  TO 'user_0d_2'@'localhost';
+-- Reload the privillages to apply changes
+FLUSH PRIVILLAGES;
