0e7dbfcbe3194e72fa71b226f0088632b461a6e8
dimka90 <dimkayilrit@gmail.com>
2023-10-18 14:17:18 +0100

    fix a bug

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creating a new user if the user  does not exit with  password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Granting privillages the select privillages to the newly created user
-GRANT SELECT  ON htb_0d_2  TO 'user_0d_2'@'localhost';
+GRANT SELECT  ON 'htb_0d_2'.*  TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Reload the privillages to apply changes
 FLUSH PRIVILLAGES;
