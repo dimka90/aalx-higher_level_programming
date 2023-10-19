338634b8683131c32c751bafa956f890e8405787
dimka90 <dimkayilrit@gmail.com>
2023-10-19 03:18:13 +0100

    formated the code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -6,7 +6,7 @@
 -- The database name will be passed as an argument of the mysql command
 
 SELECT cities.id, cities.name, states.name
-FROM cities
-    INNER JOIN states
-    ON cities.id = states.id
-ORDER BY cities.id;
+    FROM cities
+        INNER JOIN states
+        ON cities.id = states.id
+    ORDER BY cities.id;
