954759a31e64fef92c8b11e0f1ef10ab804927e6
dimka90 <dimkayilrit@gmail.com>
2023-10-19 03:11:34 +0100

    created an alias for each table

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,9 +5,9 @@
 -- You can use only one SELECT statement
 --- The database name will be passed as an argument of the mysql command
 
-SELECT cities.id, cities.name, states.name
-FROM cities
-    INNER JOIN states
-    ON cities.id = states.id
-ORDER BY cities.id;
+SELECT c.id, c.name, s.name
+FROM cities AS c
+    INNER JOIN states AS s
+    ON c.id = s.id
+ORDER BY c.id;
 
