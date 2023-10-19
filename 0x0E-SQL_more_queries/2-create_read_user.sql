3e8f027f168be304ffa22dd13dfcf7d2334a71da
dimka90 <dimkayilrit@gmail.com>
2023-10-19 03:51:39 +0100

    fix a bug for task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,12 @@
--- a script that creates the database hbtn_0d_2 and the user user_0d_2
--- Creating a new database if it does not exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Creating a new user if the user  does not exit with  password
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Granting privillages the select privillages to the newly created user
-GRANT SELECT  ON 'htb_0d_2'.*  TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Reload the privillages to apply changes
-FLUSH PRIVILLAGES;
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
