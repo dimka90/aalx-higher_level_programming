da02e199fb177c13b45d93271ae49ec845a335b0
dimka90 <dimkayilrit@gmail.com>
2023-11-14 15:42:20 +0100

    added a script that extends from the square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+/*
+Defining a an empty class
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const value = this.width;
+    this.width = this.height;
+    this.height = value;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    } else {
+      super.print();
+    }
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
