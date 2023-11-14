df11281461c3a442cb1e39435fd7c29c35ff9a2e
dimka90 <dimkayilrit@gmail.com>
2023-11-14 01:40:38 +0100

    fix some bug in task 8 by modifying the function to perform a square mulitiplication

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+const parsedInt = parseInt(args[2]);
+
+if (isNaN(parsedInt)) {
+  console.log('Missing size');
+} else {
+  for (let i = 1; i <= args[2]; i++) {
+    for(let j = 1; j <= args[2]; j ++){
+        process.stdout.write('x');
+}
+console.log();
+  }
+}
