28f515eb913be7993d3dc12d8525afcc061e96d7
dimka90 <dimkayilrit@gmail.com>
2023-11-14 15:12:36 +0100

    added a script inherits from the rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+/*
+Defining a an empty class
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const value = this.width;
+    this.width = this.height;
+    this.height = value;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
