2ecd2f001d459acdded4f2347da7a1dddf83faa8
dimka90 <dimkayilrit@gmail.com>
2023-11-14 13:59:26 +0100

    fix semistandar error

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,10 +4,10 @@ Defining a an empty class
 */
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) 
-{ this.width = w; 
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
-}
 }
 module.exports = Rectangle;
