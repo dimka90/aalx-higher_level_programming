bbb1af8c5d49bd289fc5c5fc3fdc0a86ea72dbfc
dimka90 <dimkayilrit@gmail.com>
2023-11-14 01:41:52 +0100

    fix semistanded errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -8,9 +8,9 @@ if (isNaN(parsedInt)) {
   console.log('Missing size');
 } else {
   for (let i = 1; i <= args[2]; i++) {
-    for(let j = 1; j <= args[2]; j ++){
-        process.stdout.write('x');
-}
-console.log();
+    for (let j = 1; j <= args[2]; j++) {
+      process.stdout.write('x');
+    }
+    console.log();
   }
 }
