48d09ecea691a5df1bc6430b88aaeb69b59422f5
dimka90 <dimkayilrit@gmail.com>
2023-11-17 09:42:14 +0100

    prevented the python code from executing when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,17 +3,18 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3], port = 3306)
+def fetchdata():
+    db = MySQLdb.connect(user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3], port = 3306)
+    # Create a cursor
+    cursor = db.cursor()
+    # creating a query
+    cursor.execute("SELECT * FROM states");
+    #Fetch and and print result
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
 
-# Create a cursor
-cursor = db.cursor()
+    db.close()
 
-# creating a query
-cursor.execute("SELECT * FROM states");
-
-#Fetch and and print result
-
-results = cursor.fetchall()
-
-for row in results:
-    print(row)
+if __name__ == "__main__":
+    fetchdata()
