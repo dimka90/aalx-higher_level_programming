df347c2d437971b8982b1369501462ae0c3d2451
dimka90 <dimkayilrit@gmail.com>
2023-11-19 21:20:24 +0100

    Added a script that adds a new state to the database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Script to list the first State objects
+from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+def get_state(username, password, database_name):
+    """
+    Lists the first  State objects from the specified database.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database_name (str): Name of the MySQL database.
+    """
+
+    # Create the database connection URL
+    DATABASE_URL = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                    username, password, database_name)
+
+    # Create the database engine
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+    # Bind the engine to the Base class
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    session = Session(engine)
+
+    # Create an Instance of a state
+    new_state = State(name="Louisiana")
+
+    # adding to session
+    session.add(new_state)
+
+    # commiting the new state
+    session.commit()
+    # printing the id of the newly created state
+    print(f"{new_state.id}")
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    # Check for correct number of arguments
+    if len(sys.argv) != 4:
+        print("""Usage: python script_name.py <username>
+               <password> <database_name>""")
+        sys.exit(1)
+
+    # Extract command-line arguments
+    username, password, database_name = sys.argv[1:4]
+
+    # Call the function to list states
+    get_state(username, password, database_name)
