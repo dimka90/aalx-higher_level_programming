1201eb067d19a2816ba85508340c11adb3d5e7ae
dimka90 <dimkayilrit@gmail.com>
2023-11-30 18:19:06 +0100

    remove string formatting for the value of subject

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a bash script that sends key value pairs to the server
-curl -sX POST $1 -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -L
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
