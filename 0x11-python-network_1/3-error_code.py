1c52231c25dcd0b585d16be777f6c455a87e011e
dimka90 <dimkayilrit@gmail.com>
2023-12-01 17:39:34 +0100

    added a script that reports the status of an error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Sends  request  to a server using a get request.
+
+Usage: python script.py <url>
+"""
+
+import sys
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+def success_or_error_code(url):
+    """
+    a function at takes in a URL, sends a request to the URL and displays
+    the body of the response (decoded in utf-8).
+    Args:
+        url (str): The URL to which the request will be sent.
+
+    Returns:
+        None
+    """
+    req = Request(url)
+    try:
+        with urlopen(req) as  response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print("Error code: ",e.code)
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        success_or_error_code(url)
+    except ValueError:
+        print("Usage: python script.py <url>")
+
