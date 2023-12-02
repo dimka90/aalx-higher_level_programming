746b78476f0866f30f93e37153ab3466ae3cd2b5
dimka90 <dimkayilrit@gmail.com>
2023-12-02 07:21:11 +0100

    rename function name and remove some code

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,7 @@ import sys
 import requests
 
 
-def success_or_error_code(url):
+def get_x_request_id(url):
     """
     a function at takes in a URL, sends a request to the URL and displays
     the body of the response (decoded in utf-8).
@@ -20,21 +20,13 @@ def success_or_error_code(url):
         None
     """
     response = requests.get(url)
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Retrieve the value of a specific header (e.g., 'Content-Type')
-        content_type = response.headers.get('X-Request-Id')
-        if content_type:
-            print(content_type)
-        else:
-            print('Content-Type header not present in the response')
-    else:
-        print(f"Error: {response.status_code}")
+    content_type = response.headers.get('X-Request-Id')
+    print(content_type)
 
 
 if __name__ == "__main__":
     try:
         url = sys.argv[1]
-        success_or_error_code(url)
+        get_x_request_id(url)
     except ValueError:
         print("Usage: python script.py <url>")
